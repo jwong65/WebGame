de815ffcaf23fcc183aed9d80acde893e0204fa8
jwong65 <jason.wong6507@gmail.com>
2023-11-07 18:50:15 -0500

    Added asset for simple sprite and using similar directional as placeholder

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const playchar= newPlayableChar(50,100)
 
 function newPlayableChar(x, y){
     // Character image placeholder.
-    const element = newImage('')
+    const element = newImage('assets/IdleSprite.png')
+    // Make the Z Index 1 so it is above other elements.
+    element.style.zIndex = 1;
+
+    if (direction === null){
+        element.src = 'assets/IdleSprite.png'
+    }
 }
