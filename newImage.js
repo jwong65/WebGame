5b42fc5d70bd5dab055235b0bf3a6a3ed7da0aef
jwong65 <jason.wong6507@gmail.com>
2023-11-08 01:35:45 -0500

    added alt to the image tag, still broken on Pages, trying to use Idle.png even though the file has been changed

--- newImage.js
+++ newImage.js
@@ -1,6 +1,7 @@
 function newImage(url){
     let image = document.createElement('img')
     image.src = url
+    image.alt = 'This image is broken'
     // The position of the image will be absolute for the x/y positioning
     image.style.position = 'absolute'
     image.setAttribute('id','character')
