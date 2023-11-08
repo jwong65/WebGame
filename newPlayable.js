98a64b09b183cac953b18c8ad6430b91d712df49
jwong65 <jason.wong6507@gmail.com>
2023-11-08 01:25:10 -0500

    Changed the newImage to an image instead of a gif, which only worked on local

--- newPlayable.js
+++ newPlayable.js
@@ -1,13 +1,9 @@
-var images =new Array()
-images = ['/assets/Idle/Idle1.png','assets/Idle/Idle2.png','assets/Idle/Idle3.png', 'assets/Idle/Idle4.png', 'assets/Idle5.png']
-images2 = ['/assets/walk/Right/Walk1.png','/assets/walk/Right/Walk2.png', '/assets/walk/Right/Walk3.png', '/assets/walk/Right/Walk4.png','/assets/walk/Right/Walk5.png', 'Walk6.png']
-images3 = ['/assets/walk/Left/Walk1.png','/assets/walk/Left/Walk2.png','/assets/walk/Left/Walk3.png','/assets/walk/Left/Walk4.png','/assets/walk/Left/Walk5.png','/assets/walk/Left/Walk6.png']
-// Testing animation for idle.
+
 
 function newPlayableChar(x, y){
     
     // Character image placeholder.
-    const element = newImage('/assets/IdleAnimation.gif')
+    const element = newImage('/assets/Idle.png')
     // Make the Z Index 1 so it is above other elements.
     element.style.zIndex = 1;
     element.style.id = 'character';
@@ -37,12 +33,4 @@ function newPlayableChar(x, y){
     }
    
     move(element).withArrowKeys(x, y, handleDirectionChange)
-}
-var x= 1
-function animate(images){
-    document.getElementById('character').src = images[x]
-    x++;
-    if (x===4){
-        x=1
-    }
 }
