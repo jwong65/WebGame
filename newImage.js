4abfeae29c8476a57a05f04193bae4463ab03c51
jwong65 <jason.wong6507@gmail.com>
2023-11-07 23:20:21 -0500

    Animated the character but needed to rename the assets so it would fit the array

--- newImage.js
+++ newImage.js
@@ -3,6 +3,7 @@ function newImage(url){
     image.src = url
     // The position of the image will be absolute for the x/y positioning
     image.style.position = 'absolute'
+    image.setAttribute('id','character')
     document.body.append(image)
     return image
 }
--- newPlayable.js
+++ newPlayable.js
@@ -1,16 +1,26 @@
-
+var images =new Array()
+images = ['/assets/Idle1.png','assets/Idle2.png','assets/Idle3.png', 'assets/Idle4.png', 'assets/Idle5.png']
 // Testing animation for idle.
 
 function newPlayableChar(x, y){
     
     // Character image placeholder.
-    const element = newImage('/assets/IdleSprite.png')
+    const element = newImage('/assets/Idle1.png')
     // Make the Z Index 1 so it is above other elements.
     element.style.zIndex = 1;
-    element.style.class = 'character'
+    element.style.id = 'character'
    
     // if (direction === null){
     //     element.src = './assets/IdleSprite.png'
     // }
+    setInterval('animate()',400);
     move(element).withArrowKeys(x, y)
+}
+var x= 1
+function animate(){
+    document.getElementById('character').src = images[x]
+    x++;
+    if (x===5){
+        x=1
+    }
 }
