1647ad3fb7f46b08b4fdcc833fc95f713e45937b
jwong65 <jason.wong6507@gmail.com>
2023-11-08 05:04:36 -0500

    Added spritesheets for testing

---
+++ spritesheet.js
@@ -0,0 +1,8 @@
+const spriteWidth = 80;
+const sprightHeight = 90;
+
+const canvas = document.getElementById("gameCan")
+const ctx = canvas.getContext('2d')
+
+let img = new Image();
+img.src='./assets/spritesheet/Owlet_Monster_Idle_4.png'
