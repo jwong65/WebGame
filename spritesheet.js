bf782667a3cdd8d8b1281e541c5139f0229d56e8
jwong65 <jason.wong6507@gmail.com>
2023-11-08 05:21:38 -0500

    Added flag to canvas and have each sprite on the canvas

--- spritesheet.js
+++ spritesheet.js
@@ -5,13 +5,22 @@ const canvas = document.getElementById("gameCan")
 const ctx = canvas.getContext('2d')
 
 let img = new Image();
-img.src='./assets/spritesheet/Owlet_Monster_Idle_4.png'
+img.src='./assets/spritesheet/3.png'
 img.onload= function(){
     initalize();
 }
-
+const scale = 2;
+const width = 32;
+const height = 64;
+const scaledWidth= scale* width;
+const scaledHeight = scale*height;
 
 
 function initalize(){
-    ctx.drawImage(img, 0,0, 32, 32, 0, 0, 32, 32 )
+    ctx.drawImage(img, 0,0, width, height, 0, 0, 32, 64 )
+    ctx.drawImage(img, width, 0, width, height, scaledWidth, 0, 32, 64)
+    ctx.drawImage(img, width*2, 0, width, height, scaledWidth*2, 0, 32, 64)
+    ctx.drawImage(img, width*3, 0, width, height, scaledWidth*3, 0, 32, 64)
+    ctx.drawImage(img, width*4, 0, width, height, scaledWidth*4, 0, 32, 64)
+    ctx.drawImage(img, width*5, 0, width, height, scaledWidth*5, 0, 32, 64)
 }
