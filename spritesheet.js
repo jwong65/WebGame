d254da3e212f48168494509d9543ae90a2fe1fa8
jwong65 <jason.wong6507@gmail.com>
2023-11-08 05:11:14 -0500

    Added the first frame to the canvas

--- spritesheet.js
+++ spritesheet.js
@@ -6,3 +6,12 @@ const ctx = canvas.getContext('2d')
 
 let img = new Image();
 img.src='./assets/spritesheet/Owlet_Monster_Idle_4.png'
+img.onload= function(){
+    initalize();
+}
+
+
+
+function initalize(){
+    ctx.drawImage(img, 0,0, 32, 32, 0, 0, 32, 32 )
+}
