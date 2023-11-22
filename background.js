8da451ab5580990cb4572eb26d4c02cf35651ee8
jwong65 <jason.wong6507@gmail.com>
2023-11-22 01:26:09 -0500

    Added drawObstacle as a function into the background.js file, commented out test

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 const backgroundImage = new Image()
 backgroundImage.src = './assets/backgroundAssets/Summer7.png'
+function drawObstacle(x, y, width, height){
+    cts.fillStyle = color;
+    cts.fillRect(x,y, width, outerHeight)
+
+}
+
 function drawBackground(){
     cts.drawImage(backgroundImage, 0, 0, backgroundCanvas.width, backgroundCanvas.height)
+
+    // drawObstacle(100, 200, 50, 50, 'red')
 }   
